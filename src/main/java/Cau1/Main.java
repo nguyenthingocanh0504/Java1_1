bd53121b117e5be80df4a5d4f65a2f7cd61d567e
nguyenthingocanh0504 <ngocanh05042000@gmail.com>
2023-03-07 22:12:38 +0700

    abc

---
+++ src/main/java/Cau1/Main.java
@@ -0,0 +1,10 @@
+package Cau1;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle c = new Circle();
+        c.setRadius(5);
+        System.out.println("Chu vi hinh hinh tron la: "+c.getArea());
+        System.out.println("Dien tich hinh tron la: "+c.getCircumferce());
+    }
+}
